6fb7ba708f3675bbf5faeb72a3a0ef5a44a516f4
ingenierojl <34725822+ingenierojl@users.noreply.github.com>
2022-05-24 14:26:35 -0500

    Add files via upload

---
+++ ProyectoEstructura/js/frontend.js
@@ -0,0 +1,19 @@
+function ver(n)
+{    
+    document.getElementById("subseccion"+n).style.display="block"
+
+}
+function ocultar(n)
+{
+    document.getElementById("subseccion"+n).style.display="none"
+}
+function verlateral()
+{    
+    document.getElementById("menulateral").style.display="block"  
+
+}
+function ocultarlateral()
+{    
+    document.getElementById("menulateral").style.display="none"  
+
+}
